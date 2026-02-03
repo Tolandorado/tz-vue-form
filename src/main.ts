3742a2cce922783b2d65db7b994116e208544147
Tolandorado <tonisagaz@gmail.com>
2026-02-04 01:10:18 +0300

    chore: Add persistentence plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
@@ -8,8 +9,10 @@ import './styles/main.css'
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(PrimeVue, {
     theme: {
         preset: Aura,
