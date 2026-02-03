54ede3631aabb7619974132ab246b941e39cab7e
Tolandorado <tonisagaz@gmail.com>
2026-02-03 20:03:10 +0300

    feat: Add debounce util

---
+++ src/util/debounceFn.ts
@@ -0,0 +1,8 @@
+export const debounce = <T extends (...args: any[]) => void>(fn: T, delay = 500) => {
+  let timer: ReturnType<typeof setTimeout> | null = null
+
+  return (...args: Parameters<T>) => {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => fn(...args), delay)
+  }
+}
